e275bfa92478adf1a7f316f8c6dba4bf364f0395
yunzhongfan <763748729@qq.com>
2017-10-17 00:21:27 +0800

    添加了zookeeper的分布式锁的测试服务和可恢复的配置服务测试

--- src/main/java/yun/zook/kehuifupeizhifuwu/ActiveKeyValueStore.java
+++ src/main/java/yun/zook/kehuifupeizhifuwu/ActiveKeyValueStore.java
@@ -1,26 +1,23 @@
-package yun.zook.peizhifuwu;
+package yun.zook.kehuifupeizhifuwu;
 
 import java.nio.charset.Charset;
 
-import org.apache.log4j.Logger;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.data.Stat;
 
+import yun.zook.peizhifuwu.ConnectionWatcher;
+
 public class ActiveKeyValueStore extends ConnectionWatcher {
-	Logger log = Logger.getLogger(ActiveKeyValueStore.class);
-	private static final Charset CHARSET=Charset.forName("UTF-8");
-	
+    private static final Charset CHARSET=Charset.forName("UTF-8");
     public void write(String path,String value) throws KeeperException, InterruptedException {
         Stat stat = zk.exists(path, false);
         if(stat==null){
             zk.create(path, value.getBytes(CHARSET),Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         }else{
-           Stat stac =  zk.setData(path, value.getBytes(CHARSET),-1);
-           log.info("设置数据成功,版本为："+stac.getAversion());
-            
+            zk.setData(path, value.getBytes(CHARSET),-1);
         }
     }
     public String read(String path,Watcher watch) throws KeeperException, InterruptedException{
---
+++ src/main/java/yun/zook/kehuifupeizhifuwu/ResilientConfigUpdater.java
@@ -0,0 +1,43 @@
+package yun.zook.kehuifupeizhifuwu;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.zookeeper.KeeperException;
+
+import yun.zook.peizhifuwu.ConnectionWatcher;
+
+public class ResilientConfigUpdater extends ConnectionWatcher{
+    public static final String PATH="/config";
+    private ChangedActiveKeyValueStore store;
+    private Random random=new Random();
+    
+    public ResilientConfigUpdater(String hosts) throws IOException, InterruptedException {
+        store=new ChangedActiveKeyValueStore();
+        store.connect(hosts);
+    }
+    public void run() throws InterruptedException, KeeperException{
+        while(true){
+            String value=random.nextInt(100)+"";
+            store.write(PATH,value);
+            System.out.printf("Set %s to %s\n",PATH,value);
+            TimeUnit.SECONDS.sleep(random.nextInt(10));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        while(true){
+            try {
+                ResilientConfigUpdater configUpdater = new ResilientConfigUpdater(args[0]);
+                configUpdater.run();
+            }catch (KeeperException.SessionExpiredException e) {
+                // start a new session
+            }catch (KeeperException e) {
+                // already retried ,so exit
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+}
